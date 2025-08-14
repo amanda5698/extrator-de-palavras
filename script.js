43ed894b12770d88ee89e7a1e1f2c59bc9ec14cb
amanda5698 <groff.amanda@escola.pr.gov.br>
2025-08-13 23:16:41 -0300

    aula 7

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-const BotaoMostraPalavras = document.querySelector('#botao-palavrachave');
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-BotaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
-function mostraPalavrasChave (){
-   const texto = document.querySelector('#entrada-de-texto').value;
-   const campoResultado = document.querySelector('#resultado-palavrachave');
-   const palavrasChave = processaTexto(texto);
+function mostraPalavrasChave() {
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
 
-   campoResultado.textContent = palavrasChave.join(", ");
+    campoResultado.textContent = palavrasChave.join(", ");
 }
 
-function processaTexto(texto){
+function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
-     function ordenaPalavra(p1, p2) {
+    function ordenaPalavra(p1, p2) {
         return frequencias[p2] - frequencias[p1];
     }
 
     console.log(ordenadas);
-    return ordenadas.slice(0,10);
+    return ordenadas.slice(0, 10);
 }
 
 function contaFrequencias(palavras) {
@@ -33,7 +40,16 @@ function contaFrequencias(palavras) {
             }
         }
     }
-
     return frequencias;
+}
 
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
